932310c18216699f8932aeb263225e64b8687f71
zfnmxt <zfnmxt@zfnmxt.com>
2022-03-29 15:36:15 +0000

    Remove partial

--- benchmarks/kmeans/standard/kmeans_jax.py
+++ benchmarks/kmeans/standard/kmeans_jax.py
@@ -1,6 +1,5 @@
 import gzip
 import json
-from functools import partial
 from pathlib import Path
 from time import time_ns
 
@@ -9,16 +8,19 @@ import jax
 import jax.numpy as jnp
 import numpy as np
 from benchmark import Benchmark
-from jax import grad, jvp
+from jax import grad, jvp, jit
 
 data_dir = Path(__file__).parent / "data"
 
+print(f"Running on: {jax.default_backend()}")
+
 
 class KMeans(Benchmark):
-    def __init__(self, name, runs, kind, cost_fn):
+    def __init__(self, name, runs, kind, kmeans_fn):
         self.runs = runs
         self.name = name
         self.kind = kind
+        self.kmeans_fn = kmeans_fn
 
     def prepare(self):
         _, k, max_iter, features = data_gen(self.name)
@@ -30,10 +32,8 @@ class KMeans(Benchmark):
         timings = np.zeros(runs + 1)
         for i in range(runs + 1):
             start = time_ns()
-            kmeans_fn = jax.jit(partial(kmeans, cost))
             _t, _rmse, self.objective = jax.block_until_ready(
-                kmeans_fn(self.max_iter, self.clusters, self.features)
-            )
+                self.kmeans_fn(self.max_iter, self.clusters, self.features))
             timings[i] = (time_ns() - start) / 1000
         return timings
 
@@ -50,18 +50,18 @@ class KMeans(Benchmark):
 def bench_all(runs, output, datasets=["kdd_cup", "random"], prec="f32"):
     times = {}
     for data in datasets:
-        kmeans = KMeans(data, runs, "jax", cost)
-        kmeans_vmap = KMeans(data, runs, "jax-vmap", cost_vmap)
-        kmeans.benchmark()
-        kmeans_vmap.benchmark()
+        _kmeans = KMeans(data, runs, "jax", kmeans)
+        _kmeans_vmap = KMeans(data, runs, "jax-vmap", kmeans_vmap)
+        _kmeans.benchmark()
+        _kmeans_vmap.benchmark()
         times["data/" + data] = {
-            kmeans.kind: {
-                "objective": kmeans.objective_time,
-                "objective_std": kmeans.objective_std,
+            _kmeans.kind: {
+                "objective": _kmeans.objective_time,
+                "objective_std": _kmeans.objective_std,
             },
-            kmeans_vmap.kind: {
-                "objective": kmeans_vmap.objective_time,
-                "objective_std": kmeans_vmap.objective_std,
+            _kmeans_vmap.kind: {
+                "objective": _kmeans_vmap.objective_time,
+                "objective_std": _kmeans_vmap.objective_std,
             },
         }
     with open(output, "w") as f:
@@ -93,7 +93,8 @@ def cost_vmap(features, clusters):
     return min_dist.sum()
 
 
-def kmeans(cost_fn, max_iter, clusters, features, _tolerance=1):
+@jit
+def kmeans(max_iter, clusters, features, _tolerance=1):
     tolerance = 1.0
 
     def cond(v):
@@ -102,7 +103,25 @@ def kmeans(cost_fn, max_iter, clusters, features, _tolerance=1):
 
     def body(v):
         t, rmse, clusters = v
-        f_diff = grad(partial(cost_fn, features))
+        f_diff = grad(lambda cs: cost(features,cs))
+        d, hes = jvp(f_diff, [clusters], [jnp.ones(shape=clusters.shape)])
+        new_cluster = clusters - d / hes
+        rmse = ((new_cluster - clusters) ** 2).sum()
+        return t + 1, rmse, new_cluster
+
+    return jax.lax.while_loop(cond, body, (0, float("inf"), clusters))
+
+@jit
+def kmeans_vmap(max_iter, clusters, features, _tolerance=1):
+    tolerance = 1.0
+
+    def cond(v):
+        t, rmse, _ = v
+        return jnp.logical_and(t < max_iter, rmse > tolerance)
+
+    def body(v):
+        t, rmse, clusters = v
+        f_diff = grad(lambda cs: cost(features,cs))
         d, hes = jvp(f_diff, [clusters], [jnp.ones(shape=clusters.shape)])
         new_cluster = clusters - d / hes
         rmse = ((new_cluster - clusters) ** 2).sum()
