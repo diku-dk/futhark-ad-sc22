500823234d0c95f178167fa1cd20f72ed1257351
zfnmxt <zfnmxt@zfnmxt.com>
2022-03-31 16:47:37 +0000

    Update mkdata

--- benchmarks/kmeans/standard/mkdata.sh
+++ benchmarks/kmeans/standard/mkdata.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 SEED=0
 k=1024
-d=2
-n=100000000
+d=10
+n1=2000000
+n2=3000000
 
 mkdir -p data
-futhark dataset -b -s $SEED -g i32 -g ${k}i64 -g 10i32 -g [$n][$d]f32 | gzip > data/k$k-d$d-n$n.in.gz;
+futhark dataset -b -s $SEED -g i32 -g ${k}i64 -g 10i32 -g [$n][$d]f32 | gzip > data/k$k-d$d-n$n1.in.gz;
+futhark dataset -b -s $SEED -g i32 -g ${k}i64 -g 10i32 -g [$n][$d]f32 | gzip > data/k$k-d$d-n$n2.in.gz;
