45149155362ef2dd27a13b159a3fecdda68f16c3
zfnmxt <zfnmxt@zfnmxt.com>
2022-04-14 03:57:19 +0200

    Clean-up.

--- setup-python-amd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ ! -d ".venv" ] 
-then 
-  $PYTHON -m venv .venv
-fi
-source .venv/bin/activate
-pip install --upgrade -r requirements-amd.txt  --extra-index-url https://download.pytorch.org/whl/rocm4.2
-
-
